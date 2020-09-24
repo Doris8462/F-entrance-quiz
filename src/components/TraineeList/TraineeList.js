d88e5323fb493141c6c61661eb2a7f9019b1a2d7
Doris8462 <liandou123>
2020-09-24 15:09:56 +0800

    [huangyunjie] feat:display traineeList

--- src/components/TraineeList/TraineeList.js
+++ src/components/TraineeList/TraineeList.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import './TraineeList.scss'
 
 export default class TraineeList extends Component {
+  constructor() {
+    super();
+    this.state = { trainees: [] };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8080/trainees?grouped=false')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          trainees: data,
+        });
+      });
+  }
+
   render() {
     return (
       <div>
         <h2>学员列表</h2>
         <div className="trainee-list">
-          <div className="name-item"> </div>
-          <input className="name-item" value="+" />
+          {Object.keys(this.state.trainees).map((key) => (
+            <div className="name-item" key={this.state.trainees[key].id}>
+              {`${this.state.trainees[key].id}. ${this.state.trainees[key].name}`}
+            </div>
+          ))}
+          <button className="name-item add-trainee">+添加学员</button>
         </div>
       </div>
     );
